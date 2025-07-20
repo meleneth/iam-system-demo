ae9effcd180a9933be5377ed5c6be514aa8d01d3
Lord Meleneth <meleneth@moxie>
2025-07-20 13:47:53 +0000

    Add user-create-queue-worker and configure it to run 5 instances

---
+++ user-service/app/workers/user_create_queue_worker.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require 'aws-sdk-sqs'
+require 'json'
+
+class UserCreateQueueWorker
+  POLL_INTERVAL = 1 # seconds
+  MAX_MESSAGES = 10
+
+  def initialize(queue_url: "http://eventstream:4566/000000000000/user_create")
+    @sqs = Aws::SQS::Client.new(
+      region: 'us-east-1',
+      endpoint: 'http://eventstream:4566',
+      access_key_id: 'fake',
+      secret_access_key: 'fake'
+    )
+    @queue_url = queue_url
+  end
+
+  def run
+    puts "[user-queue-worker] starting loop on #{@queue_url}"
+    loop do
+      resp = @sqs.receive_message(
+        queue_url: @queue_url,
+        max_number_of_messages: MAX_MESSAGES,
+        wait_time_seconds: 1
+      )
+
+      if resp.messages.empty?
+        sleep POLL_INTERVAL
+        next
+      end
+
+      resp.messages.each do |msg|
+        process(msg)
+      end
+    end
+  end
+
+  def process(msg)
+    raw = JSON.parse(msg.body)
+    body = false
+
+    if raw["Type"] == "Notification" && raw["Message"]
+      body = JSON.parse(raw["Message"])
+    else
+      body = raw
+    end
+
+
+    unless body["type"] == "demo.user.create"
+      puts "[user-queue-worker] unknown type: #{body["type"]}"
+      return delete(msg)
+    end
+
+    user_data = body["user"]
+    email     = user_data["email"]
+    account_id = user_data["account_id"]
+    user_id = user_data["id"]
+
+    user = User.find_by(email: email)
+    if user
+      puts "[user-queue-worker] already exists: #{email}"
+    else
+      user = User.create!(
+        id: user_id,
+        email: email,
+        account_id: account_id
+      )
+      puts "[user-queue-worker] created: #{email} (user_id=#{user.id})"
+    end
+
+    delete(msg)
+  rescue => e
+    puts "[user-queue-worker] error processing message: #{e.class}: #{e.message}"
+    puts e.backtrace.join("\n")
+    # Optionally send to DLQ or log error
+  end
+
+  def delete(msg)
+    @sqs.delete_message(
+      queue_url: @queue_url,
+      receipt_handle: msg.receipt_handle
+    )
+  end
+end
